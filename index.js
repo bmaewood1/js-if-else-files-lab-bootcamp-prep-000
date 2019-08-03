55ea39798ff354269c2d4101b57d5e18afb0b260
Bailey Wood <woodbmae@gmail.com>
2019-08-03 17:53:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+<<<<<<< HEAD
 var name = "Susan"
 var height = "70"
 
+=======
+var name = "Susan";
+var height = "74"
+if (name === "Susan") {
+  console.log("The name is correct")
+} else {
+  console.log("Expected the name to be Susan")
+}
+>>>>>>> 4b5c9554cca9ca65aa5edf4d49ee2061275f2076
 
 
 // Don't worry about this
